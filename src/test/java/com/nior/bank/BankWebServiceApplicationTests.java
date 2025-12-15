98bfe981ddf1eef14e339d8a94835c4ce5d738bc
DemoFake99 <demofake2122@gmail.com>
2025-12-15 13:45:53 +0530

    Initial Commit

---
+++ src/test/java/com/nior/bank/BankWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nior.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
